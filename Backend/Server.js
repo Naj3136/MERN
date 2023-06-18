1a6c77fa0dd9dea5c7ef120a6eb9672e3eb2c15e
MUHAMMED SALIH K <muhdsalih13@gmail.com>
2023-06-18 12:44:42 +0530

    update

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,20 +1,37 @@
 const express = require('express');
 const app = express();
-const PORT =4578
-const mongoose =require('mongoose');
+const PORT = 4533 //PORT
+const mongoose = require('mongoose'); //mongoose for DB
+const cors = require('cors'); // to remore cor issue
+app.use(cors())  // cor policy activation
+app.use(express.json()); // to render json req from frontend
+app.use(express.urlencoded({extended:true})); // to render form data from frontend
 
 mongoose.connect("mongodb+srv://Najla:3136@cluster0.c9gsepb.mongodb.net/")
 .then(()=>{console.log("MongoDB connected successfully")})
 .catch(()=>{console.log("Error connecting to mongoDB")})
 
-app.get('/',(req,res)=>{
-    res.send('Server is running')
+
+// CRUD operation 
+// C-Create - POST 
+//R-READ-GET
+//U-Update -PUT
+//D-Delete - DELETE
+
+app.post('/addData', (req, res) => {
+    try {
+
+        let item = req.body
+        console.log(item)
+
+
+
+    } catch (error) {
+        res.send(error);
+    }
 })
 
-app.get('/about',(req,res)=>{
-    res.send('About')
-})
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 });
